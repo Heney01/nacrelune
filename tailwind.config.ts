6810b1c72f442d59dd7ff3928bd3a4a19eff10a3
Hugo Andreu <andreuhugo@gmail.com>
2025-07-28 16:26:51 +0000

    Est-ce qu'il n'y a pas un problème de serveur en US et Europe ? Je suis

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,16 +55,6 @@ export default {
           '4': 'hsl(var(--chart-4))',
           '5': 'hsl(var(--chart-5))',
         },
-        sidebar: {
-          DEFAULT: 'hsl(var(--background))',
-          foreground: 'hsl(var(--foreground))',
-          primary: 'hsl(var(--primary))',
-          'primary-foreground': 'hsl(var(--primary-foreground))',
-          accent: 'hsl(var(--accent))',
-          'accent-foreground': 'hsl(var(--accent-foreground))',
-          border: 'hsl(var(--border))',
-          ring: 'hsl(var(--ring))',
-        },
       },
       borderRadius: {
         lg: 'var(--radius)',
