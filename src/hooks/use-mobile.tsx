845ce9c8d3b39e4ee37cf4d010490f134e80c8bd
Hugo Andreu <andreuhugo@gmail.com>
2025-07-28 11:40:12 +0000

    Error: Maximum update depth exceeded. This can happen when a component c

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
@@ -6,13 +7,20 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const checkIsMobile = () => {
+        return window.innerWidth < MOBILE_BREAKPOINT;
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Set the initial value
+    setIsMobile(checkIsMobile());
+
+    const handleResize = () => {
+        setIsMobile(checkIsMobile());
+    }
+
+    window.addEventListener("resize", handleResize)
+    
+    return () => window.removeEventListener("resize", handleResize)
   }, [])
 
   return !!isMobile
