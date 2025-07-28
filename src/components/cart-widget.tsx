4014213b91d58060c349546e4461a21b86657dc6
Hugo Andreu <andreuhugo@gmail.com>
2025-07-28 12:32:49 +0000

    Attention, l'icone qui indique le nombre d'element dans la panier n'est

--- src/components/cart-widget.tsx
+++ src/components/cart-widget.tsx
@@ -21,7 +21,7 @@ export function CartWidget() {
 
   if (!hasMounted) {
     return (
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon" className="relative">
             <ShoppingCart className="h-6 w-6" />
             <span className="sr-only">{t('cart_title')}</span>
         </Button>
@@ -30,7 +30,7 @@ export function CartWidget() {
 
   return (
     <CartSheet>
-        <Button variant="ghost" size="icon">
+        <Button variant="ghost" size="icon" className="relative">
             <ShoppingCart className="h-6 w-6" />
             {itemCount > 0 && (
                 <span className="absolute top-0 right-0 bg-primary text-primary-foreground text-xs font-bold rounded-full h-4 w-4 flex items-center justify-center transform translate-x-1/4 -translate-y-1/4">
