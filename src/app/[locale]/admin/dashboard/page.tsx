49beadd3d5f4d5d2c36b2d03a21c8537d7a1780e
Hugo Andreu <andreuhugo@gmail.com>
2025-07-29 12:34:22 +0000

    Dans le dossier [locale] j'ai plusieurs fichiers page.tsx et layout.tsx

--- src/app/[locale]/admin/dashboard/page.tsx
+++ src/app/[locale]/admin/dashboard/page.tsx
@@ -1,4 +1,3 @@
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { NacreluneLogo } from "@/components/icons";
 import { Button } from "@/components/ui/button";
@@ -18,44 +17,46 @@ export default async function AdminDashboard({ params }: { params: { locale: str
   const jewelryTypes = await getJewelryTypesAndModels(JEWELRY_TYPES_INFO);
   
   return (
-    <div className="flex-col md:flex">
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <NacreluneLogo className="h-8 w-auto" />
-          <div className="ml-auto flex items-center space-x-4">
-            <form action={logout}>
-                <input type="hidden" name="locale" value={params.locale} />
-                <Button variant="ghost">
-                    Se déconnecter
-                </Button>
-            </form>
+    <div className="bg-muted/40 min-h-screen">
+      <div className="flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <NacreluneLogo className="h-8 w-auto" />
+            <div className="ml-auto flex items-center space-x-4">
+              <form action={logout}>
+                  <input type="hidden" name="locale" value={params.locale} />
+                  <Button variant="ghost">
+                      Se déconnecter
+                  </Button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Tableau de bord</h2>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">Tableau de bord</h2>
+          </div>
+          <Tabs defaultValue="models" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="models">
+                <Gem className="mr-2 h-4 w-4" />
+                Gérer les modèles
+              </TabsTrigger>
+              <TabsTrigger value="charms" disabled>
+                <Wrench className="mr-2 h-4 w-4" />
+                Gérer les breloques
+              </TabsTrigger>
+              <TabsTrigger value="users" disabled>
+                  <User className="mr-2 h-4 w-4" />
+                  Gérer les utilisateurs
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="models" className="space-y-4">
+              <ModelsManager initialJewelryTypes={jewelryTypes} locale={params.locale} />
+            </TabsContent>
+          </Tabs>
+          <TestDeleteButton />
         </div>
-        <Tabs defaultValue="models" className="space-y-4">
-          <TabsList>
-            <TabsTrigger value="models">
-              <Gem className="mr-2 h-4 w-4" />
-              Gérer les modèles
-            </TabsTrigger>
-            <TabsTrigger value="charms" disabled>
-              <Wrench className="mr-2 h-4 w-4" />
-              Gérer les breloques
-            </TabsTrigger>
-            <TabsTrigger value="users" disabled>
-                <User className="mr-2 h-4 w-4" />
-                Gérer les utilisateurs
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="models" className="space-y-4">
-            <ModelsManager initialJewelryTypes={jewelryTypes} locale={params.locale} />
-          </TabsContent>
-        </Tabs>
-        <TestDeleteButton />
       </div>
     </div>
   );
