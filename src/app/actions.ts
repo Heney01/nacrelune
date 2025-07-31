a83dbc3fd7942dfeff1704cae6a4982984e20c76
Hugo Andreu <andreuhugo@gmail.com>
2025-07-31 12:23:33 +0000

    J'ai une erreur de build Failed to compile. ./src/app/actions.ts:148:13

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -122,8 +122,8 @@ export async function saveModel(prevState: any, formData: FormData): Promise<{ s
     const name = formData.get('name') as string;
     const price = parseFloat(formData.get('price') as string);
     const quantity = parseInt(formData.get('quantity') as string, 10);
-    const width = parseFloat(formData.get('width') as string) || null;
-    const height = parseFloat(formData.get('height') as string) || null;
+    const width = parseFloat(formData.get('width') as string) || undefined;
+    const height = parseFloat(formData.get('height') as string) || undefined;
     const reorderUrl = formData.get('reorderUrl') as string;
     const locale = formData.get('locale') as string || 'fr';
 
@@ -317,8 +317,8 @@ export async function saveCharm(prevState: any, formData: FormData): Promise<{ s
     const description = formData.get('description') as string;
     const price = parseFloat(formData.get('price') as string);
     const quantity = parseInt(formData.get('quantity') as string, 10);
-    const width = parseFloat(formData.get('width') as string) || null;
-    const height = parseFloat(formData.get('height') as string) || null;
+    const width = parseFloat(formData.get('width') as string) || undefined;
+    const height = parseFloat(formData.get('height') as string) || undefined;
     const categoryIds = formData.getAll('categoryIds') as string[];
     const reorderUrl = formData.get('reorderUrl') as string;
     const locale = formData.get('locale') as string || 'fr';
