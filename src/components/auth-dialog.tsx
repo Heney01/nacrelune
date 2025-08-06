f48dbcd67c03d060f5ebd06a0b919822cda7b1c4
Hugo Andreu <andreuhugo@gmail.com>
2025-08-06 08:37:20 +0000

    Quand j'ouvre le dialogue de connexion j'ai ça dans la console app-index

--- src/components/auth-dialog.tsx
+++ src/components/auth-dialog.tsx
@@ -2,17 +2,26 @@
 'use client';
 
 import React from 'react';
-import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { useAuthDialog } from '@/hooks/use-auth-dialog';
 import { LoginForm } from './login-form';
 import { SignUpForm } from './signup-form';
+import { useTranslations } from '@/hooks/use-translations';
 
 export function AuthDialog() {
   const { isOpen, close, view } = useAuthDialog();
+  const t = useTranslations('Auth');
+
+  const title = view === 'login' ? t('user_login_title') : t('user_signup_title');
+  const description = view === 'login' ? t('user_login_description') : t('user_signup_description');
 
   return (
     <Dialog open={isOpen} onOpenChange={close}>
-      <DialogContent className="sm:max-w-md p-0">
+      <DialogContent className="sm:max-w-md p-0" aria-describedby={description}>
+         <DialogHeader className="hidden">
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
         {view === 'login' ? <LoginForm /> : <SignUpForm />}
       </DialogContent>
     </Dialog>
