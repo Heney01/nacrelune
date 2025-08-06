152949853fab7c4be57a0448089a58d5c775b1d6
Hugo Andreu <andreuhugo@gmail.com>
2025-08-06 19:59:07 +0000

    Non pour le header su dite il faut garder l'ancienne image. Celle que je

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="fr">
         <head>
-            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💎</text></svg>" />
+            <link rel="icon" href="/logo.png" type="image/png" />
             <meta 
               name="viewport" 
               content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" 
