59ff5b103164e144cba786ffdc7858c5009bb80f
Hugo Andreu <andreuhugo@gmail.com>
2025-08-06 17:06:51 +0000

    Voici mon numéro de SIREN: 920861424

--- src/app/[locale]/legal/mentions-legales/page.tsx
+++ src/app/[locale]/legal/mentions-legales/page.tsx
@@ -8,8 +8,8 @@ export default function LegalNoticePage() {
       <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Atelier à bijoux l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
 
       <h2>Édition du site</h2>
-      <p>Le présent site, accessible à l’URL www.atelierabijoux.com (le « Site »), est édité par :</p>
       <p><strong>Hugo Andreu</strong>, entreprise individuelle (Micro-entreprise), dont le siège social est situé au 33 avenue de la Martheline 13009 Marseille.</p>
+      <p>Immatriculée sous le numéro SIREN 920861424.</p>
       <p>Le numéro individuel TVA est : FR34920861424.</p>
       
       <h2>Hébergement</h2>
